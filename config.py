e3f8905139e6292e1f2af0b57b825338ad0a526d
shuvo803 <mdabulkhair675@gmail.com>
2025-08-22 14:21:26 +0600

    Update config.py

--- config.py
+++ config.py
@@ -1,16 +1,16 @@
 import os, re
 
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "7769997460:AAGote37S-jOib2dSDc3PU-FRu0TBYb-YDg")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7525776533:AAFbPEiZIVUJ2NIJU2GGW25okECAwQUHdKU")
 API_ID = int(os.environ.get("API_ID", "23864777"))
 API_HASH = os.environ.get("API_HASH", "1ad9abca4b87cee505e4ed3b1a811665")
 OWNER = int(os.environ.get("OWNER", "6509218702"))
-BOT_USERNAME = os.environ.get('BOT_USERNAME', "TS_4gbRename_bot")
+BOT_USERNAME = os.environ.get('BOT_USERNAME', "Rename_S_Bot")
 
 FORCE_SUBS = os.environ.get("FORCE_SUBS", "S_MovieChannel")
-LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002442180033)
+LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002342857145)
 
-DB_URL = os.environ.get("DB_URL", "mongodb+srv://STRenameBot:STRenameBot@cluster0.p7bubb0.mongodb.net/?retryWrites=true&w=majority")
+DB_URL = os.environ.get("DB_URL", "mongodb+srv://shuvokuku:shuvokuku@cluster0.2mp65mn.mongodb.net/?retryWrites=true&w=majority")
 DB_NAME = os.environ.get("DB_NAME", "renamebot-premium")
 
 STRING = os.environ.get("STRING", "BQFsJckAez_Nzdm9tljjuRWu5FQJKexs1XpZ742oGh-OA65Nps5c1cGfHN1dAZmsseClyvC7ZPYp2J9I3KsxbcepQVxxfm_v9vJS5JsvaXJxCOq5up9TcCl7OOpl5kLB_mw_OGsFwv2RbnAX5QjTOuII3j2ZVvyEcJz1Hmo7Tzi95Dtvy-BnrFdG-eEee6FTiiUztiPXb2emaIeCZmo7y8zThNroi-hQpQ4G09ABDOLP0osaf0fu8ThiRbBLVhXtZ-keJklSLqTRZNw8AQ2IOFVjAP2jXLj2qKUIgGyj7mMJ7mk5WKKlM6qnY_KMf-7yPYPzti5ndBclV9TIzfZeGm4zlamOXgAAAAGD-suOAA")
