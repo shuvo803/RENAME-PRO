bb78abb02aae5dd4f344f465c5619a606fad26b3
shuvo803 <mdabulkhair675@gmail.com>
2025-08-22 14:24:34 +0600

    Update config.py

--- config.py
+++ config.py
@@ -13,7 +13,7 @@ LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002342857145)
 DB_URL = os.environ.get("DB_URL", "mongodb+srv://shuvokuku:shuvokuku@cluster0.2mp65mn.mongodb.net/?retryWrites=true&w=majority")
 DB_NAME = os.environ.get("DB_NAME", "renamebot-premium")
 
-STRING = os.environ.get("STRING", "BQFsJckAez_Nzdm9tljjuRWu5FQJKexs1XpZ742oGh-OA65Nps5c1cGfHN1dAZmsseClyvC7ZPYp2J9I3KsxbcepQVxxfm_v9vJS5JsvaXJxCOq5up9TcCl7OOpl5kLB_mw_OGsFwv2RbnAX5QjTOuII3j2ZVvyEcJz1Hmo7Tzi95Dtvy-BnrFdG-eEee6FTiiUztiPXb2emaIeCZmo7y8zThNroi-hQpQ4G09ABDOLP0osaf0fu8ThiRbBLVhXtZ-keJklSLqTRZNw8AQ2IOFVjAP2jXLj2qKUIgGyj7mMJ7mk5WKKlM6qnY_KMf-7yPYPzti5ndBclV9TIzfZeGm4zlamOXgAAAAGD-suOAA")
+STRING = os.environ.get("STRING", "ShuvoBot")
 BOT_PIC = os.environ.get("BOT_PIC", "https://graph.org/file/cca849a2f63053fa3f622.jpg")
 
 
@@ -22,6 +22,6 @@ BOT_PIC = os.environ.get("BOT_PIC", "https://graph.org/file/cca849a2f63053fa3f62
 
 # if you need to add verify system then dm me on telegram
 
-SHORTNER_URL = os.environ.get("SHORTNER_URL", "modijiurl.com")
-SHORTNER_API = os.environ.get("SHORTNER_API", "8c082f290e876fc34f1ab066c4775c44a1dedead")
+SHORTNER_URL = os.environ.get("SHORTNER_URL", "shortxlinks.com")
+SHORTNER_API = os.environ.get("SHORTNER_API", "67564248fa5c25a8a2f7d3a85a0aec068feef56a")
 TOKEN_TIMEOUT = os.environ.get("TOKEN_TIMEOUT", "12")
